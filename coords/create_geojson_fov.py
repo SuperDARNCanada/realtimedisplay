3e7200bf6bf734408d7cf9844c2f4ef7dcf69853
Keith Kotyk <keithkotyk@gmail.com>
2017-09-27 10:41:56 -0600

    Modified FOV script to output 15km and 45km FOVS

--- coords/create_geojson_fov.py
+++ coords/create_geojson_fov.py
@@ -4,19 +4,7 @@ from davitpy import pydarn
 from datetime import datetime
 import sys
 
-def main():
-	radar = sys.argv[1]
-	nbeams = int(sys.argv[2])
-	ngates = int(sys.argv[3])
-
-	date = datetime.now() 
-
-	radId = pydarn.radar.network().getRadarByCode(radar).id
-	site = pydarn.radar.site(radId=radId, dt=date)
-
-	myFov = pydarn.radar.radFov.fov(site=site, rsep=45, ngates=ngates, nbeams=nbeams, coords = 'geo', date_time=date)
-
-
+def make_geojson(nbeams,ngates,myFov):
 	polys = []
 	cs = []
 
@@ -31,13 +19,36 @@ def main():
 			coords.append((myFov.lonFull[beam,gate],myFov.latFull[beam,gate]))
 
 			polys.append(geojson.Feature(geometry=geojson.Polygon([coords]),id=str(beam).zfill(2)+str(gate).zfill(2)))
-		
+
 	featureCollection = geojson.FeatureCollection(polys)
 	validation = geojson.is_valid(featureCollection)
 
-	filename = sys.argv[4]
-	with open(filename,'w') as outfile:
-		geojson.dump(featureCollection,outfile)
+	return featureCollection
+
+def main():
+	radar = sys.argv[1]
+	nbeams = int(sys.argv[2])
+	ngates = int(sys.argv[3])
+
+	date = datetime.now()
+
+	radId = pydarn.radar.network().getRadarByCode(radar).id
+	site = pydarn.radar.site(radId=radId, dt=date)
+
+	myFov_15km = pydarn.radar.radFov.fov(site=site, rsep=15, ngates=ngates, nbeams=nbeams, coords = 'geo', date_time=date)
+	myFov_45km = pydarn.radar.radFov.fov(site=site, rsep=45, ngates=ngates, nbeams=nbeams, coords = 'geo', date_time=date)
+
+	geo_15km = make_geojson(nbeams,ngates,myFov_15km)
+	geo_45km = make_geojson(nbeams,ngates,myFov_45km)
+
+	filename_1 = radar + "geojson_15km.json"
+	filename_2 = radar + "geojson_45km.json"
+
+	with open(filename_1,'w') as outfile:
+		geojson.dump(geo_15km,outfile)
+
+	with open(filename_2,'w') as outfile:
+		geojson.dump(geo_45km,outfile)
 
 if __name__ == "__main__":
 	main()
