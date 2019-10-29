561994ea9d77de3b490bc9c0fd2a1a7c86cc129e
Keith Kotyk <keithkotyk@gmail.com>
2019-10-29 16:11:05 -0600

    changes to fov creation

--- coords/create_geojson_fov.py
+++ coords/create_geojson_fov.py
@@ -4,19 +4,23 @@ from davitpy import pydarn
 from datetime import datetime
 import sys
 
-def make_geojson(nbeams,ngates,myFov):
+def make_geojson(site,rsep,nbeams,ngates,lagfr):
+
+	date = datetime.now()
+	FOV = pydarn.radar.radFov.fov(site=site, rsep=rsep, ngates=ngates, nbeams=nbeams, coords = 'geo', date_time=date)
+
+
 	polys = []
 	cs = []
-
 	for beam in range(nbeams):
 		for gate in range(ngates):
 			coords = []
 	        #print beam, gate,myFov.latFull[beam,gate],myFov.latFull[beam+1,gate],myFov.latFull[beam,gate+1], myFov.latFull[beam+1,gate+1],myFov.lonFull[beam,gate],myFov.lonFull[beam+1,j],myFov.lonFull[i,j+1], myFov.lonFull[i+1,gate+1]
-			coords.append((myFov.lonFull[beam,gate],myFov.latFull[beam,gate]))
-			coords.append((myFov.lonFull[beam+1,gate],myFov.latFull[beam+1,gate]))
-			coords.append((myFov.lonFull[beam+1,gate+1],myFov.latFull[beam+1,gate+1]))
-			coords.append((myFov.lonFull[beam,gate+1],myFov.latFull[beam,gate+1]))
-			coords.append((myFov.lonFull[beam,gate],myFov.latFull[beam,gate]))
+			coords.append((FOV.lonFull[beam,gate],FOV.latFull[beam,gate]))
+			coords.append((FOV.lonFull[beam+1,gate],FOV.latFull[beam+1,gate]))
+			coords.append((FOV.lonFull[beam+1,gate+1],FOV.latFull[beam+1,gate+1]))
+			coords.append((FOV.lonFull[beam,gate+1],FOV.latFull[beam,gate+1]))
+			coords.append((FOV.lonFull[beam,gate],FOV.latFull[beam,gate]))
 
 			polys.append(geojson.Feature(geometry=geojson.Polygon([coords]),id=str(beam).zfill(2)+str(gate).zfill(2)))
 
@@ -30,7 +34,7 @@ def main():
 	nbeams = int(sys.argv[2])
 	ngates = int(sys.argv[3])
 
-	date = datetime.now()
+
 
 	radId = pydarn.radar.network().getRadarByCode(radar).id
 	site = pydarn.radar.site(radId=radId, dt=date)
